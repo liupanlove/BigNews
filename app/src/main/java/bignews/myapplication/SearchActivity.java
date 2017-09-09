0f6ec38fa2e33d884ac212962baa31b6cf9fdd58
liupan15 <1942093769@qq.com>
2017-09-09 15:19:14 +0800

    change SearchView

--- app/src/main/java/bignews/myapplication/SearchActivity.java
+++ app/src/main/java/bignews/myapplication/SearchActivity.java
@@ -1,20 +1,30 @@
 package bignews.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.SearchView;
 
 public class SearchActivity extends Activity
 {
-    private SearchView searchView1;
+    private SearchView searchView;
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search);
-        searchView1 = (SearchView) findViewById(R.id.searchview1);
+
+        Intent intent = getIntent();
+        String query = intent.getStringExtra("querycontent");
+        searchView = (SearchView) findViewById(R.id.searchview1);
+        searchView.setQuery(query, true);
+        searchView.setIconifiedByDefault(true);
+        searchView.setFocusable(true);
+        searchView.setIconified(false);
+        searchView.requestFocusFromTouch();
+        /*searchView1 = (SearchView) findViewById(R.id.searchview1);
         searchView1.setFocusable(true);
         searchView1.setFocusableInTouchMode(true);
         searchView1.requestFocus();
-        searchView1.requestFocusFromTouch();
+        searchView1.requestFocusFromTouch();*/
     }
 }
