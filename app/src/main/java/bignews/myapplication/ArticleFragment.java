f0b79e68836c9d94ecfd0c1997402e52a02fd141
liupan15 <1942093769@qq.com>
2017-09-10 18:41:40 +0800

    share

--- app/src/main/java/bignews/myapplication/ArticleFragment.java
+++ app/src/main/java/bignews/myapplication/ArticleFragment.java
@@ -1,23 +1,9 @@
-/*
- * Copyright (C) 2012 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package bignews.myapplication;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.support.annotation.Nullable;
 import android.os.Bundle;
 import android.support.v4.view.LayoutInflaterCompat;
@@ -26,15 +12,20 @@ import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
 
 import bignews.myapplication.db.DAO;
 import bignews.myapplication.db.DAOParam;
 
-public class ArticleFragment extends Activity {
+public class ArticleFragment extends Activity implements View.OnClickListener{
     final static String ARG_POSITION = "position";
     int mCurrentPosition = -1;
     DAO dao = DAO.getInstance();
@@ -42,6 +33,12 @@ public class ArticleFragment extends Activity {
     PopupWindow pop;
     TextView textView;
     View view;
+    ImageButton weChatFriend;
+    //ImageButton weChatZone;
+    ImageButton qq;
+    ImageButton weiBo;
+    Button cancel;
+    Button collect;
     //Toolbar toolBar;
     LinearLayout linearLayout;
     @Override
@@ -62,11 +59,61 @@ public class ArticleFragment extends Activity {
                 pop.showAtLocation(linearLayout, Gravity.BOTTOM, 0, 0);
             }
         });
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                pop.dismiss();
+            }
+        });
     }
 
     @Override
     public void onStart() {
         super.onStart();
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.cancel:
+                pop.dismiss();
+                return;
+            case R.id.collect:
+                Toast.makeText(getApplicationContext(), "已收藏", Toast.LENGTH_SHORT);
+                return;
+        }
+        String pakName = "";
+
+        switch(v.getId())
+        {
+            case R.id.weChatFriend:
+                pakName = "com.tencent.mm";
+                break;
+            case R.id.weiBo:
+                pakName = "com.sina.weibo";
+                break;
+            case R.id.qq:
+                pakName = "com.tencent.mobileqq";
+                break;
+            /*case R.id.weChatZone:
+                pakName = "com.qzone";
+                break;*/
+        }
+        Intent intent = new Intent(Intent.ACTION_SEND);
+
+        File f = new File("/storage/emulated/0/DCIM/Camera/IMG_20161125_094646.jpg");
+        Uri u = Uri.fromFile(f);
+        intent.putExtra(intent.EXTRA_STREAM, u);
+        intent.setType("image/*");
+        intent.putExtra("body", "233");
+
+        intent.putExtra(Intent.EXTRA_SUBJECT, "这里是分享主题");
+        intent.putExtra(Intent.EXTRA_TEXT, "这里是分享内容");
+
+        intent.setPackage(pakName);
+        this.startActivity(Intent.createChooser(intent, "分享到"));
     }
 
     public void updateArticleView(int position)
@@ -75,26 +122,39 @@ public class ArticleFragment extends Activity {
         article.setText(dao.getNews(DAOParam.fromNewsId(position)).blockingGet().newsTitle);
         mCurrentPosition = position;
     }
-
-    @Override
+    /*@Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         // Save the current article selection in case we need to recreate the fragment
         //outState.putInt(ARG_POSITION, mCurrentPosition);
-    }
+    }*/
 
     private void initView()
     {
         LayoutInflater inflater = LayoutInflater.from(this);
-        view = inflater.inflate(R.layout.popup, null);
+        view = inflater.inflate(R.layout.popup, null);       // new View
+
         textView = (TextView) findViewById(R.id.article);
         linearLayout = (LinearLayout) findViewById(R.id.linear);
+        weChatFriend = (ImageButton) view.findViewById(R.id.weChatFriend);
+        //weChatZone = (ImageButton) view.findViewById(R.id.weChatZone);
+        qq = (ImageButton) view.findViewById(R.id.qq);
+        weiBo = (ImageButton) view.findViewById(R.id.weiBo);
+        cancel = (Button) view.findViewById(R.id.cancel);
+        collect = (Button) view.findViewById(R.id.collect);
         //toolBar = (Toolbar) findViewById(R.id.toolbar1);
     }
     private void initData()
     {
-        pop = new PopupWindow(view, ViewPager.LayoutParams.MATCH_PARENT, 700, true); // 有点问题
+        pop = new PopupWindow(view, ViewPager.LayoutParams.MATCH_PARENT, 750, true); // 有点问题
         //pop.setAnimationStyle(R.style.MenuAnimationFade);
         pop.setBackgroundDrawable(new BitmapDrawable());
+        //pop.setContentView(view);
+        cancel.setOnClickListener(this);
+        collect.setOnClickListener(this);
+        weChatFriend.setOnClickListener(this);
+        //weChatZone.setOnClickListener(this);
+        qq.setOnClickListener(this);
+        weiBo.setOnClickListener(this);
     }
 }
--- app/src/main/java/bignews/myapplication/TestActivity.java
+++ app/src/main/java/bignews/myapplication/TestActivity.java
@@ -1,43 +1,73 @@
 package bignews.myapplication;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ListView;
-import android.widget.SearchView;
-import android.widget.SimpleAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+public class TestActivity extends Activity implements View.OnClickListener{
 
-public class TestActivity extends Activity
-{
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    private PopupWindow mPopWindow;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.test);
+        setContentView(R.layout.main);
 
-        ListView listView = (ListView) findViewById(R.id.listView);
-        //listView.addHeaderView(line());
-        List<Map<String, Object>> listItems = new ArrayList<Map<String, Object>>();
+        Button btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showPopupWindow();
+            }
+        });
+    }
 
-        for(int i = 0; i < 10; ++i)
-        {
-            Map<String, Object> map = new HashMap<String, Object>();
-            map.put("image", R.drawable.home);
-            map.put("title", "hello");
-            listItems.add(map);
+    private void showPopupWindow() {
+        //设置contentView
+        View contentView = LayoutInflater.from(TestActivity.this).inflate(R.layout.popuplayout, null);
+        mPopWindow = new PopupWindow(contentView,
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, true);
+        mPopWindow.setContentView(contentView);
+        //设置各个控件的点击响应
+        TextView tv1 = (TextView)contentView.findViewById(R.id.pop_computer);
+        TextView tv2 = (TextView)contentView.findViewById(R.id.pop_financial);
+        TextView tv3 = (TextView)contentView.findViewById(R.id.pop_manage);
+        tv1.setOnClickListener(this);
+        tv2.setOnClickListener(this);
+        tv3.setOnClickListener(this);
+        //显示PopupWindow
+        View rootview = LayoutInflater.from(TestActivity.this).inflate(R.layout.main, null);
+        mPopWindow.showAtLocation(rootview, Gravity.BOTTOM, 0, 0);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id){
+            case R.id.pop_computer:{
+                Toast.makeText(this,"clicked computer",Toast.LENGTH_SHORT).show();
+                mPopWindow.dismiss();
+            }
+            break;
+            case R.id.pop_financial:{
+                Toast.makeText(this,"clicked financial",Toast.LENGTH_SHORT).show();
+                mPopWindow.dismiss();
+            }
+            break;
+            case R.id.pop_manage:{
+                Toast.makeText(this,"clicked manage",Toast.LENGTH_SHORT).show();
+                mPopWindow.dismiss();
+            }
+            break;
         }
-
-        SimpleAdapter adapter = new SimpleAdapter(this, listItems, R.layout.search_item,
-                new String[]{"title", "image"}, new int[]{R.id.title, R.id.image});
-        listView.setAdapter(adapter);
-        /*searchView1 = (SearchView) findViewById(R.id.searchview1);
-        searchView1.setFocusable(true);
-        searchView1.setFocusableInTouchMode(true);
-        searchView1.requestFocus();
-        searchView1.requestFocusFromTouch();*/
     }
 }
